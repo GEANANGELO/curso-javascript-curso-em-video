7bdb621162469ba42a3a8d7e47aa50b11f909be4
GEANANGELO <geanangelodasilva@hotmail.com>
2022-08-29 00:50:21 -0300

    add 5-condicoes.js ao repositorio

---
+++ 5-condicoes.js
@@ -0,0 +1,17 @@
+var vel = 61
+console.log(`a velocidade do seu carro é ${vel}km/h `)
+if(vel >60){
+    console.log(`vocÊ ultrapassou o limite maximo permitido de 60km/h`)
+}else{
+    console.log('siga em frente')
+}
+
+var pais = 'brasil';
+console.log(`vivendo em ${pais}`);
+if (pais != 'brasil'){
+    console.log('voce é estrangeiro');
+} else (
+    console.log('voce é brasileiro')
+)
+//f8 para executar no console
+
