2d851f58cadc317935efaab471a7c1feddb9601f
GEANANGELO <geanangelodasilva@hotmail.com>
2022-09-01 11:21:39 -0300

    adicionando arquivo 13-condicao-while.js ao repositorio

---
+++ 13-condicao-while.js
@@ -0,0 +1,16 @@
+var c = 1
+/* ABAIXO ESTRUTURA WHILE
+while(c <= 5){
+    console.log(`oi ${c}`)
+    c++
+}
+*/
+//ABAIXO ESTRUTURA FOR
+do{
+    console.log(`oi ${c}`)
+    c++
+} while(c <=6)
+
+
+
+
