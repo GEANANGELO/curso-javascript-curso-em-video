1696436a3a4ea1e1e51169b596eb8ff24b51137a
GEANANGELO <geanangelodasilva@hotmail.com>
2022-09-05 10:30:47 -0300

    add aquivo 14-repeticao-FOR.js ao repositorio

---
+++ 14-repeticao-FOR.js
@@ -0,0 +1,26 @@
+//estrurura WHILE
+/*
+var c =1
+while(c <= 10){
+    console.log(`ola ${c}`)
+    c++
+}
+*/
+
+//estrurura DO WHILE
+/*
+var c =1
+do {
+    console.log(c)
+    c++
+} while(c <= 5)
+*/
+
+
+//estrurura FOR
+console.log('vai começar')
+for(var c = 1;c <= 10; c++){
+    console.log(`ola ${c}`)
+} 
+console.log('fim')
+
